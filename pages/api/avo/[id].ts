30fd80f5048ec1eabd6e4232d1be056671372361
Arnold Restrepo Hernandez <arnold.restrepohernandez@gmail.com>
2021-12-21 01:10:43 -0500

    Add cors and improve API

--- pages/api/avo/[id].ts
+++ pages/api/avo/[id].ts
@@ -4,8 +4,15 @@ import enablePublicAccess from '@cors'
 
 const AvoDetail = async (request: NextApiRequest, response: NextApiResponse) => {
   try {
-
+    await enablePublicAccess(request, response);
+    const db = new DataBase();
+    const id = request.query.id;
+    const avo = await db.getDataById(id as string);
+    response.status(200).json(avo);
   } catch (error) {
-    
+    console.error(error);
+    response.status(400).end();
   }
 }
+
+export default AvoDetail;
--- pages/api/avo/index.ts
+++ pages/api/avo/index.ts
@@ -1,13 +1,19 @@
-import { IncomingMessage, ServerResponse } from 'http';
+import { NextApiRequest, NextApiResponse } from 'next';
 import DataBase from "@database";
+import enablePublicAccess from '@cors'
 
-const allAvos = async (request: IncomingMessage, response: ServerResponse) => {
-  const db = new DataBase();
-  const allEntries = await db.getData();
-  const length = allEntries.length;
-  response.statusCode = 200;
-  response.setHeader('Content-Type', 'application/json');
-  response.end(JSON.stringify({data: allEntries, length}));
+const allAvos = async (request: NextApiRequest, response: NextApiResponse) => {
+  try {
+    await enablePublicAccess(request, response);
+    const db = new DataBase();
+    const allEntries = await db.getData();
+    const length = allEntries.length;
+    response.status(200).json({ data: allEntries, length });
+  } catch (error) { 
+    console.error(error);
+    response.status(500).end();
+  }
+  
 }
 
 export default allAvos;
