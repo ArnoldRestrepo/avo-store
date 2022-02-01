3ee4d702c560ec76b14c154358950c1fa5efded5
Arnold Restrepo Hernandez <arnold.restrepohernandez@gmail.com>
2022-01-31 23:47:44 -0500

    GetStaticPaths and GetStatisProps

--- pages/product/[productId].tsx
+++ pages/product/[productId].tsx
@@ -1,24 +1,35 @@
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
 import { Layout } from '@components/Layout'
 import fetch from 'isomorphic-unfetch'
 import ProductSummary from '@components/ProductSummary'
+import { GetStaticPaths, GetStaticProps } from 'next'
 
-const ProductItem = () => {
-  const {
-    query: { productId },
-  } = useRouter()
-  const [product, setProduct] = useState<TProduct>()
-  useEffect(() => {
-    if (productId) {
-      fetch(`/api/avo/${productId}`)
-        .then((res) => res.json())
-        .then((data) => {
-          console.log(data)
-          setProduct(data)
-        })
-    }
-  }, [productId])
+export const getStaticPaths = async () => {
+  const response = await fetch('https://platzi-avocados.vercel.app/api/avo')
+  const { data: productList }: TAPIAvoResponse = await response.json()
+
+  const paths = productList.map(({ id: productId }) => ({
+    params: {
+      productId,
+    },
+  }))
+
+  return {
+    paths,
+    fallback: false,
+  }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const id = params?.productId as string
+  const response = await fetch(`https://avo-store-plum.vercel.app/api/avo/${id}`)
+  const product: TProduct = await response.json()
+
+  return {
+    props: { product },
+  }
+}
+
+const ProductItem = ({ product }: { product: TProduct }) => {
   return <Layout>{product == null ? null : <ProductSummary product={product} />}</Layout>
 }
 
